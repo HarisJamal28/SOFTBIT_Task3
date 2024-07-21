961915912aed8a265a36dc0402a72d7c067121a7
Haris Jamal <143813651+HarisJamal28@users.noreply.github.com>
2024-07-21 22:35:18 +0500

    Add files via upload

---
+++ logic.js
@@ -0,0 +1,12 @@
+
+
+
+// function Show(id){
+//     var x = document.getElementById(id.toString().toLowerCase());
+//     x.style.display = 'flex';
+//     if(x.style.display=='flex'){
+//         x.style.display = 'none';
+//     }else{
+//         x.style.display = 'flex';
+//     }
+// }
